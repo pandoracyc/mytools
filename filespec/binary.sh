d8913bb77ce2a70225619754c146d643e699ac5a
pandoracyc <pandoracyc>
2013-01-16 08:42:42 +0900

    update

--- filespec/binary.sh
+++ filespec/binary.sh
@@ -11,60 +11,75 @@
 #=========================================================================
 # MAIN
 #=========================================================================
+OUTPUT_HTML=test.html
+PREFIX=sample
+EXTENSION=dat
+HEAD_CSV=sample_head.csv
+BODY_CSV=sample_body.csv
+LOG_FILE=log
+
 echo read file : sample.dat
 xxd sample.dat
 echo 
 
-echo run read.rb
-./read.rb >sample.log
+echo "--------------------------------------------------------------------------------"
+echo run read.rb ${PREFIX}_${EXTENSION}.conf
+cat ${PREFIX}_${EXTENSION}.conf
+./read.rb ${PREFIX}_${EXTENSION}.conf >LOG_FILE
 echo result is $?
+echo "--------------------------------------------------------------------------------"
 echo
 
+#--------------------------------------
+# OUTPUT_HTML
+#--------------------------------------
+echo make $OUTPUT_HTML
 
-echo sample_head.csv
-echo "---------------------------------------------------"
-cat > test.html <<EOF
+#--------------------------------------
+# read sample_head.csv
+#--------------------------------------
+cat > $OUTPUT_HTML <<EOF
 <H2>HEAD</H2>
 <table border=1>
 EOF
 while read line
 do 
-	echo -en '<tr>\n<td>' >>test.html
-	echo $line|sed 's%,%</td>\n<td>%g' >>test.html
-	echo -e '</td>\n</tr>' >>test.html
-	echo >>test.html
-	echo >>test.html
-done <sample_head.csv
+	echo -en '<tr>\n<td>' >>$OUTPUT_HTML
+	echo $line|sed 's%,%</td>\n<td>%g' >>$OUTPUT_HTML
+	echo -e '</td>\n</tr>' >>$OUTPUT_HTML
+	echo >>$OUTPUT_HTML
+	echo >>$OUTPUT_HTML
+done <$HEAD_CSV
 echo 
-cat >> test.html <<EOF
+cat >> $OUTPUT_HTML <<EOF
 </table>
 
 
 
 EOF
-echo "---------------------------------------------------"
 
 
-
-
-echo sample_body.csv
-echo "---------------------------------------------------"
-cat >> test.html <<EOF
+#--------------------------------------
+# read sample_body.csv
+#--------------------------------------
+cat >> $OUTPUT_HTML <<EOF
 <H2>BODY</H2>
 <table border=1>
 EOF
 while read line
 do 
-	echo -en '<tr>\n<td>' >>test.html
-	echo $line|sed 's%,%</td>\n<td>%g' >>test.html
-	echo -e '</td>\n</tr>' >>test.html
-	echo >>test.html
-	echo >>test.html
-done <sample_body.csv
-cat >> test.html <<EOF
+	echo -en '<tr>\n<td>' >>$OUTPUT_HTML
+	echo $line|sed 's%,%</td>\n<td>%g' >>$OUTPUT_HTML
+	echo -e '</td>\n</tr>' >>$OUTPUT_HTML
+	echo >>$OUTPUT_HTML
+	echo >>$OUTPUT_HTML
+done <$BODY_CSV
+cat >> $OUTPUT_HTML <<EOF
 </table>
 EOF
-echo "---------------------------------------------------"
+
+
+
 #echo run write.rb
 #./write.rb >>sample.log
 #echo result is $?
